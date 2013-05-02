6282bbdb108ceae1db79d843e55a75cd2946b12f
Kaspar Pedersen <kasparp@me.com>
2013-05-02 12:14:19 +0200

    asdf

--- script/build_riack.sh
+++ script/build_riack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd riack/
-cmake -D BUILD_SHARED_LIBS:BOOL=OFF -D RIAK_TEST_SERVER_IP=$2 -D RIAK_TEST_SERVER_PB_PORT=$1 src/
-make
+cmake -D BUILD_SHARED_LIBS:BOOL=OFF -D RIAK_TEST_SERVER_IP=$1 -D RIAK_TEST_SERVER_PB_PORT=$2 src/
+make clean all
 exit $?
