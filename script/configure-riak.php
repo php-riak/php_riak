708d59a736c0e909189d1ac36b5d3aa4221b1b3d
Kaspar Pedersen <kpn@trifork.com>
2013-10-07 13:49:08 +0200

    update dep

--- script/configure-riak.php
+++ script/configure-riak.php
@@ -1,2 +1,14 @@
 <?php
-// TODO
+//  {riak_search, [
+//                %% To enable Search functionality set this 'true'.
+//                {enabled, false}
+//               ]},
+//-->
+//  {riak_search, [
+//                %% To enable Search functionality set this 'true'.
+//                {enabled, true}
+//               ]},
+
+//            {storage_backend, riak_kv_bitcask_backend},
+//-->
+//            {storage_backend, riak_kv_eleveldb_backend},
